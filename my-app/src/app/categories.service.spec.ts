d57fb4676c2f7078f4efc069a74f0dd08e9e0643
c.giulioni <c.giulioni@loccioni.com>
2018-11-16 13:59:40 +0100

    Adds test to check addCategory methods

--- my-app/src/app/categories.service.spec.ts
+++ my-app/src/app/categories.service.spec.ts
@@ -12,6 +12,7 @@ fdescribe('CategoriesService', () => {
   beforeEach(() => {
     categoriesApiServiceStub = <CategoriesApiService>{};
     categoriesApiServiceStub.loadCategories = () => null;
+    categoriesApiServiceStub.addCategory = (category: CategoryDTO) => null;
 
     sut = new CategoriesService(categoriesApiServiceStub);
   });
@@ -23,7 +24,7 @@ fdescribe('CategoriesService', () => {
     scheduler.flush();
   });
 
-  it('should categories be fill by Category objects after loadCategories', () => {
+  it('should categories emits array with cat1 and cat2 after loadCategories() called', () => {
     const scheduler = getTestScheduler();
     const dtos: CategoryDTO[] = [
       {
@@ -38,17 +39,52 @@ fdescribe('CategoriesService', () => {
       },
     ];
 
-    const cat1 = new Category();
-    const cat2 = new Category();
-    cat1.mapFromDTO(dtos[0]);
-    cat2.mapFromDTO(dtos[1]);
+    const cat1 = new Category({
+      code: 'code1',
+      icon: 'icon1',
+      name: 'name1'
+    });
+    const cat2 = new Category({
+      code: 'code2',
+      icon: 'icon2',
+      name: 'name2'
+    });
+
+
 
     spyOn(categoriesApiServiceStub, 'loadCategories').and
       .callFake(function () { return of(dtos, scheduler); });
 
-     scheduler.schedule(() => sut.loadCategories(), 10);
+    scheduler.schedule(() => sut.loadCategories(), 10);
 
     const expected = cold('ab', { a: [], b: [cat1, cat2] });
+    expect(sut.categories).toBeObservable(expected);
+
+    // activate the scheduler so the test can properly run!
+    scheduler.flush();
+  });
+
+
+  it('should categories emits 2 events if addCategory is called twice', () => {
+    const scheduler = getTestScheduler();
+    const cat1 = new Category({
+      code: 'code1',
+      icon: 'icon1',
+      name: 'name1'
+    });
+    const cat2 = new Category({
+      code: 'code2',
+      icon: 'icon2',
+      name: 'name2'
+    });
+
+    spyOn(categoriesApiServiceStub, 'addCategory').and
+      .callFake(function (category: CategoryDTO) { return of({}); });
+
+    scheduler.schedule(() => sut.addCategory(cat1), 10);
+    scheduler.schedule(() => sut.addCategory(cat2), 20);
+
+    const expected = cold('abc', { a: [],  b: [cat1], c: [cat1, cat2]});
     expect(sut.categories).toBeObservable(expected);
 
     // activate the scheduler so the test can properly run!
