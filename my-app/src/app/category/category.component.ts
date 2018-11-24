5e45e8cb29fe95986b2360c3efb42ce59f195890
qina83@gmail.com <qina83@gmail.com>
2018-11-24 14:55:23 +0100

    Adds return to main categories page after submit of edit
    It still remain problem of how manage error.

--- my-app/src/app/category/category.component.ts
+++ my-app/src/app/category/category.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CategoriesService } from '../categories.service';
 import { Category } from '../category';
-import { Observable } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -13,14 +12,22 @@ export class CategoryComponent {
 
   public categoryToEdit: Category;
 
-  constructor(private categoriesService: CategoriesService, private route: ActivatedRoute) {
+  constructor(private categoriesService: CategoriesService,
+    private router: Router,
+    private route: ActivatedRoute) {
     const code: string = this.route.snapshot.params['code'];
     this.categoryToEdit = categoriesService.getCagetoryByCode(code);
   }
 
   onSubmit(form: any): void {
-    console.log('you submitted value:', form);
-    //how to check that submit si completed?
+    console.log('you submitted value:', form, this.categoryToEdit);
+    const category = this.categoryToEdit
+      .setDirection(form['direction'])
+      .setName(form['name'])
+      .setIcon(form['icon']);
+     this.categoriesService.substituteCategory(category);
+     this.router.navigate(['/categoryPage']);
+    // how to check that submit si completed?
   }
 
 
